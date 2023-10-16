270edff9d74dedcc1fc8df6c0362f1791cebc10e
Lillian <137111941+lillianedwards@users.noreply.github.com>
2023-10-16 17:15:20 -0500

    Create file structure

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const { connect, connection } = require('mongoose');
+
+
+const connectionString = 
+process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+connect(connectionString)
+
+module.exports = connection;
